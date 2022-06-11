aba1571944b554dcea05d437431fb17ec0b26fbb
F-A-B-R-I-Z-I-O <fabriziofagiolo18@gmail.com>
2022-06-11 23:50:27 +0200

    load-model(pre-train)

--- mobileAge/src/app/tab1/tab1.page.ts
+++ mobileAge/src/app/tab1/tab1.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as tf from '@tensorflow/tfjs';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page {
-
+export class Tab1Page implements OnInit {
   constructor() {}
 
+  async loadModel() {
+    const model = await tf.loadLayersModel('file://src/assets/reset50.json');
+  }
+
+  ngOnInit() {
+    this.loadModel();
+    console.log('Load Model');
+  }
 }
